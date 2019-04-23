3818b016630ccc73f57622c3bf4935f3f3b2bb0c
DESKTOP-7JRTRPT\Sandli <sandlimatharoo24@gmail.com>
2019-04-23 21:24:45 +0530

    Add steps and master form along with state handling logic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MasterForm from './MasterForm';
+
+function App() {
+  return (
+    <div className="App">
+    <MasterForm/>
+    </div>
+  );
+}
+
+export default App;
